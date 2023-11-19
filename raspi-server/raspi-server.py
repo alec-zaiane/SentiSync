286c3ce8d66e5355167631d12b68f2370cefe9f4
Alec <alec.zaiane@gmail.com>
2023-11-18 23:26:36 -0700

    cleanup

--- raspi-server/raspi-server.py
+++ raspi-server/raspi-server.py
@@ -27,7 +27,7 @@ GPIO.setup(17, GPIO.OUT)
 GPIO.output(17, GPIO.LOW)
 
 
-# Function to receive data from the separate computer
+# Function to receive data from the separate computer, use this to receive commands for haptic feedback
 def receive_data():
     while True:
         data = connection.recv(1024)  # Adjust buffer size as needed
@@ -35,44 +35,6 @@ def receive_data():
             break
         # Process the received data as needed
         print("Received data:", data.decode("utf-8"))
-        data = data.decode("utf-8")
-        if data == "beep":
-            print("beep")
-            GPIO.output(17, GPIO.HIGH)
-            time.sleep(0.1)
-            GPIO.output(17, GPIO.LOW)
-        elif data == "beep beep":
-            print("beep beep")
-            GPIO.output(17, GPIO.HIGH)
-            time.sleep(0.1)
-            GPIO.output(17, GPIO.LOW)
-            time.sleep(0.1)
-            GPIO.output(17, GPIO.HIGH)
-            time.sleep(0.1)
-            GPIO.output(17, GPIO.LOW)
-        elif data == "hbd":
-            BEEPER_PIN = 17
-
-            # Define the notes for the "Happy Birthday" song
-            notes = [
-                ('C4', 0.5), ('C4', 0.5), ('D4', 1.0), ('C4', 1.0), ('F4', 1.0), ('E4', 2.0),
-                ('C4', 0.5), ('C4', 0.5), ('D4', 1.0), ('C4', 1.0), ('G4', 1.0), ('F4', 2.0),
-                ('C4', 0.5), ('C4', 0.5), ('C5', 1.0), ('A4', 1.0), ('F4', 1.0), ('E4', 1.0), ('D4', 2.0),
-                ('Bb4', 0.5), ('Bb4', 0.5), ('A4', 1.0), ('F4', 1.0), ('G4', 1.0), ('F4', 2.0)
-            ]
-
-            # Define the frequencies for each note
-            frequencies = {
-                'C4': 261.63, 'D4': 293.66, 'E4': 329.63, 'F4': 349.23, 'G4': 392.00, 'A4': 440.00, 'Bb4': 466.16, 'B4': 493.88, 'C5': 523.25
-            }
-
-            pwm = GPIO.PWM(BEEPER_PIN, 100)
-            for note, duration in notes:
-                pwm.ChangeFrequency(frequencies[note])
-                pwm.start(50)
-                time.sleep(duration)
-                pwm.stop()
-                time.sleep(0.05)
 
 # Start a separate thread for receiving data
 receive_thread = threading.Thread(target=receive_data)
@@ -88,13 +50,8 @@ try:
 
             # Serialize the frame
             data = pickle.dumps(frame)
-            #print(data)
-            #print("=====")
             # Pack the serialized frame and send it
             size = struct.pack("=L", len(data))
-            #print(type(size))
-            #print(size)
-            #print(struct.unpack("=L", size))
             connection.sendall(size + data)
 
 finally:
