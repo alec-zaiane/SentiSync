b3c4207a0462c8150afae0992b306b4335c5c6c6
Alec <alec.zaiane@gmail.com>
2023-11-19 06:09:38 -0700

    Refactor screen processing and add server stop functionality

--- hardware-interconnect/macos-server.py
+++ hardware-interconnect/macos-server.py
@@ -13,8 +13,24 @@ print(f"[yellow]Local IP address: {local_ip}[/yellow]")
 
 assert local_ip != "", "Please set the local IP address of the server."
 
+kill_threads = False
 
-def serve_facetime():
+
+def process_screen(frame):
+    # process the frame here,
+    # INPUT: black and white image
+    # OUTPUT: cropped, resized, processed image
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    cropin1 = (144, 104, -142, -148) # distance from left, top, right bottom
+    framecrop1 = frame[cropin1[1]:cropin1[3], cropin1[0]:cropin1[2]]
+    cropin2 = (1589, 747, -40, -168)
+    framecrop2 = frame[cropin2[1]:cropin2[3], cropin2[0]:cropin2[2]]
+    result1, framecrop1 = cv2.imencode(".jpg", framecrop1, [int(cv2.IMWRITE_JPEG_QUALITY), 70])
+    result2, framecrop2 = cv2.imencode(".jpg", framecrop2, [int(cv2.IMWRITE_JPEG_QUALITY), 90]) # higher quality for tiny image
+    return (result1 and result2), (framecrop1, framecrop2)
+
+def serve_screen():
     global local_ip
     ft_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     ft_socket.bind((local_ip, 5555))
@@ -22,16 +38,14 @@ def serve_facetime():
     print(f"Waiting for FaceTime connection... (local IP: {local_ip}, port: 5555)")
     ft_connection, ft_client_address = ft_socket.accept()
     print(f"FaceTime connection established with {ft_client_address}.")
-    while True:
-        # capture whole screen, TODO crop to just the FaceTime window
+    while not kill_threads:
         img = pyautogui.screenshot()
         frame = np.array(img)
-        # rescale to 720p and compress before sending it
-        frame = cv2.resize(frame, (1280, 720))
-        result, frame = cv2.imencode(".jpg", frame, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
+        # process then send the frames
+        result, frames = process_screen(frame)
         if result:
             # Serialize the frame
-            data = pickle.dumps(frame)
+            data = pickle.dumps(frames)
             # Pack the serialized frame and send it
             size = struct.pack("=L", len(data))
             ft_connection.sendall(size + data)
@@ -65,7 +79,7 @@ def serve_audio():
                     input=True,
                     frames_per_buffer=CHUNK)
 
-    while True:
+    while not kill_threads:
         # Read audio data from the stream
         data = stream.read(CHUNK)
         # Send audio data over the connection
@@ -85,7 +99,7 @@ def receive_data():
     print(f"Waiting for data connection... (local IP: {local_ip}, port: 5557)")
     data_connection, data_client_address = data_socket.accept()
     print(f"Data connection established with {data_client_address}.")
-    while True:
+    while not kill_threads:
         data = data_connection.recv(1024)  # Adjust buffer size as needed
         if not data:
             print("data connection closed")
@@ -98,7 +112,7 @@ def receive_data():
 def main():
     print("Starting server...")
     time.sleep(0.1)
-    facetime_svr = threading.Thread(target=serve_facetime)
+    facetime_svr = threading.Thread(target=serve_screen)
     facetime_svr.start()
     time.sleep(0.1)
     audio_svr = threading.Thread(target=serve_audio)
@@ -106,7 +120,16 @@ def main():
     time.sleep(0.1)
     data_svr = threading.Thread(target=receive_data)
     data_svr.start()
+    time.sleep(0.1)
     print("[green]Server started.[/green]")
+    print("Press ctrl-c to stop the server.")
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print("Stopping server...")
+            kill_threads = True
+            break
     # wait for all threads to finish
     facetime_svr.join()
     audio_svr.join()
