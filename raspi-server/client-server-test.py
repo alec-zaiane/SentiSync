566def50266efeace1fb143f240559b44b912704
Alec <alec.zaiane@gmail.com>
2023-11-18 23:03:01 -0700

    multithread frame reception

--- raspi-server/client-server-test.py
+++ raspi-server/client-server-test.py
@@ -4,12 +4,14 @@ import pickle
 import struct
 import numpy as np
 import threading
+import queue
 
 # Create a socket connection
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('10.42.0.1', 5555))
+client_socket.connect(('192.168.1.100', 5555))
 payload_size = struct.calcsize("=L")
-data = b""
+
+frame_queue = queue.Queue()
 
 # Function to send data to the Raspberry Pi
 def send_data():
@@ -21,8 +23,8 @@ def send_data():
 send_thread = threading.Thread(target=send_data)
 send_thread.start()
 
-try:
-    ctr = 0
+def receive_frame():
+    data = b""
     while True:
         # Receive the size of the serialized frame
         while len(data) < payload_size:
@@ -30,19 +32,29 @@ try:
         packed_size = data[:payload_size]
         data = data[payload_size:]
         msg_size = struct.unpack("=L", packed_size)[0]
-
         # Receive the rest of serialized frame
         while len(data) < msg_size:
             data += client_socket.recv(4096)
 
         frame_data = data[:msg_size]
         data = data[msg_size:]
-
         frame_data = pickle.loads(frame_data)
         frame_array = np.frombuffer(frame_data, dtype=np.uint8)
-        frame = cv2.imdecode(frame_array, flags=cv2.IMREAD_COLOR)
+        output_frame = cv2.imdecode(frame_array, flags=cv2.IMREAD_COLOR)
+        frame_queue.put(output_frame)
+
+# Start a separate thread for receiving data
+receive_thread = threading.Thread(target=receive_frame)
+receive_thread.start()
+
+try:
+    ctr = 0
+    while True:
+        # get the next frame from the queue (skip a few frames if needed)
+        while frame_queue.qsize() > 1:
+            frame_queue.get()
+        frame = frame_queue.get()
         
-        #print(frame)
         cv2.imshow('Received Video', frame)
         cv2.waitKey(1)
 
