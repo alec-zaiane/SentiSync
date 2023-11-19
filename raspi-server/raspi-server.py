525b21c947ac80c7029f3edca0350a3aa6f93935
Alec <alec.zaiane@gmail.com>
2023-11-18 23:03:10 -0700

    beep gpio

--- raspi-server/raspi-server.py
+++ raspi-server/raspi-server.py
@@ -3,11 +3,13 @@ import socket
 import pickle
 import struct
 import threading
+import RPi.GPIO as GPIO
+import time
 
 # Create a socket connection
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # bindip is the IP address of the computer running the server program, get it using ipconfig
-bindip = "192.168.137.85"
+bindip = "192.168.1.100"
 server_socket.bind((bindip, 5555))  # Change the IP and port as needed
 server_socket.listen(0)
 connection, client_address = server_socket.accept()
@@ -18,6 +20,13 @@ cap = cv2.VideoCapture("/dev/video0")  # Use 0 for the default camera
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 405)
 
+# setup GPIO
+# 17 is beep pin
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.output(17, GPIO.LOW)
+
+
 # Function to receive data from the separate computer
 def receive_data():
     while True:
@@ -26,6 +35,22 @@ def receive_data():
             break
         # Process the received data as needed
         print("Received data:", data.decode("utf-8"))
+        data = data.decode("utf-8")
+        if data == "beep":
+            print("beep")
+            GPIO.output(17, GPIO.HIGH)
+            time.sleep(0.1)
+            GPIO.output(17, GPIO.LOW)
+        elif data == "beep beep":
+            print("beep beep")
+            GPIO.output(17, GPIO.HIGH)
+            time.sleep(0.1)
+            GPIO.output(17, GPIO.LOW)
+            time.sleep(0.1)
+            GPIO.output(17, GPIO.HIGH)
+            time.sleep(0.1)
+            GPIO.output(17, GPIO.LOW)
+
 
 # Start a separate thread for receiving data
 receive_thread = threading.Thread(target=receive_data)
