69bf2cf050a63478f67587d3000eaa270390c143
Alec <alec.zaiane@gmail.com>
2023-11-18 18:24:22 -0700

    Update webcam frame size and image compression

--- raspi-server/raspi-server.py
+++ raspi-server/raspi-server.py
@@ -7,7 +7,7 @@ import threading
 # Create a socket connection
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # bindip is the IP address of the computer running the server program, get it using ipconfig
-bindip = "10.42.0.1"
+bindip = "192.168.137.85"
 server_socket.bind((bindip, 5555))  # Change the IP and port as needed
 server_socket.listen(0)
 connection, client_address = server_socket.accept()
@@ -15,6 +15,8 @@ print("Connection from", client_address)
 
 # Open the webcam
 cap = cv2.VideoCapture(0)  # Use 0 for the default camera
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 405)
 
 # Function to receive data from the separate computer
 def receive_data():
@@ -32,19 +34,24 @@ receive_thread.start()
 try:
     while True:
         # Read a frame from the webcam
-        #ret, frame = cap.read()
-        frame = "Test hello :) yippee" * 500
+        ret, frame = cap.read()
+        #frame = "Test hello :) yippee"
+        result, frame = cv2.imencode(".jpg", frame, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
+        if result:
 
-        # Serialize the frame
-        data = pickle.dumps(frame)
-
-        # Pack the serialized frame and send it
-        size = struct.pack("L", len(data))
-        print(type(size))
-        print(size)
-        connection.sendall(size + data)
+            # Serialize the frame
+            data = pickle.dumps(frame)
+            #print(data)
+            #print("=====")
+            # Pack the serialized frame and send it
+            size = struct.pack("=L", len(data))
+            #print(type(size))
+            #print(size)
+            #print(struct.unpack("=L", size))
+            connection.sendall(size + data)
 
 finally:
     cap.release()
     connection.close()
     server_socket.close()
+
